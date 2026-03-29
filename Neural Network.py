f55e34b1623e7b56a56b8bafdd756785641bd4f3
rrri01 <rraine001@gmail.com>
2026-03-28 19:57:16 -0600

    use pandas to create a datafram from housing.csv

---
+++ Neural Network.py
@@ -0,0 +1,6 @@
+# github repo link for my own reference: https://github.com/rrri01/DATA-221-Group-Project-Raines-Stuff
+
+import pandas as pd
+
+housing_data = pd.read_csv('housing.csv', delimiter=',')
+print(housing_data)
