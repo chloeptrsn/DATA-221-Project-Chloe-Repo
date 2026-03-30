e72b5256c562dcdd45042d39fdcec6d34e98bf8f
rrri01 <rraine001@gmail.com>
2026-03-30 10:50:52 -0600

    split into testing and training data, also import some packages

--- Neural Network.py
+++ Neural Network.py
@@ -3,8 +3,14 @@
 
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
+import numpy as np
+
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+
 housing_data = pd.read_csv('housing.csv', delimiter=',')
-print(housing_data)
 
 # the next few lines were just to figure out what the categorical options were
 # d = {}
@@ -25,10 +31,14 @@ print(housing_data)
 # NEAR OCEAN
 # ISLAND
 
-from tensorflow.keras.utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
 
 housing_data = housing_data.replace({"NEAR BAY": 0, "<1H OCEAN": 1, "INLAND": 2, "NEAR OCEAN": 3, "ISLAND": 4})
 
-print(housing_data)
+
+# creates feature matrix X of all columns except "median_house_value" and create label vector y as "median_house_value"
+X = housing_data.drop("median_house_value", axis=1)
+y = housing_data["median_house_value"]
+
+# splits the data into training data and testing data
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
