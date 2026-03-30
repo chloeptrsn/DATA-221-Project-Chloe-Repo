44793da1b6cf3d8d718819b387b55fd92bea4bc9
NganTranAlex <tranthanhngan.2906.alex@gmail.com>
2026-03-30 12:50:25 -0600

    mapping categorical variable in column ocean_proximity

--- California House Price Prediction.py
+++ California House Price Prediction.py
@@ -7,6 +7,17 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 # load the dataset/ csv file
 california_house_prices = pd.read_csv("C:/Codefile/housing file/housing.csv")
 
+# --- Preprocessing data ---
 # fill in the missing values (the missing values are usually present in total_bedrooms)
 california_house_prices["total_bedrooms"] = california_house_prices["total_bedrooms"].fillna(california_house_prices["total_bedrooms"].median())
 
+# mapping (converting categorical to numerical)
+mapping = {
+    "INLAND": 0,
+    "NEAR OCEAN": 1,
+    "NEAR BAY": 2,
+    "ISLAND": 3,
+    "<1H OCEAN": 4
+}
+
+california_house_prices["ocean_proximity"] = california_house_prices["ocean_proximity"].map(mapping)
