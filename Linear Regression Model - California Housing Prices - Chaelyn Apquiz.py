1007c8608f8b2d76206ab0302fb1bcf3bdc8db6c
cha-lin2005 <chaelynapquiz@gmail.com>
2026-03-30 18:15:55 -0600

    added predictions, fixed variable naming, fixed linear model

--- Linear Regression Model - California Housing Prices - Chaelyn Apquiz.py
+++ Linear Regression Model - California Housing Prices - Chaelyn Apquiz.py
@@ -13,10 +13,7 @@ california_house_prices = pd.read_csv("housing.csv")
 # find the median and fill in the missing values which can be found in the total_bedrooms
 california_house_prices["total_bedrooms"] = (california_house_prices["total_bedrooms"].fillna(california_house_prices["total_bedrooms"].median()))
 
-# converting categorical to numerical instead of mapping
-california_house_prices = pd.get_dummies(california_house_prices, columns=["ocean_proximity"])
-
-# converting categorical data which is the ocean_proximity column
+# converting categorical to numerical instead of mapping: one-hot encoding
 california_house_prices = pd.get_dummies(california_house_prices, columns=["ocean_proximity"])
 
 #splitting the features and target (x and y)
@@ -24,14 +21,12 @@ feature_matrix = california_house_prices.drop("median_house_value", axis=1)
 target_prices = california_house_prices["median_house_value"]
 
 #training data, test split 70/30
-features_train, features_test, prices_train, prices_test = train_test_split(feature_matrix, target_prices, test_size=0.33, random_state=42)
+feature_train, feature_test, target_train, target_test = train_test_split(feature_matrix, target_prices, test_size=0.3, random_state=42)
 
-# scaling the values
-scaled_values = StandardScaler()
-features_train_scaled = scaled_values.fit_transform(features_train)
-features_test_scaled = scaled_values.transform(features_test)
+# training linear model
+linear_regression_model = LinearRegression()
+linear_regression_model.fit(feature_train, target_train)
 
-# Training Linear Regression Model
-model = LinearRegression()
-model.fit(features_train_scaled, target_prices)
+# predictions
+predicted_values = linear_regression_model.predict(feature_test)
 #git push -u origin main
