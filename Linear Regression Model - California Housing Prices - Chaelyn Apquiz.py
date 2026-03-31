a5507fb9bcc9446c544a043207b8ac1fb1bc08d5
Chaelyn <chaelynapquiz@gmail.com>
2026-03-30 19:10:58 -0600

    removed unused importrs

--- Linear Regression Model - California Housing Prices - Chaelyn Apquiz.py
+++ Linear Regression Model - California Housing Prices - Chaelyn Apquiz.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import root_mean_squared_error, mean_absolute_error, r2_score
 
